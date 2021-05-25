05ffb8f2c2b34fdb546b47b87067aa51875e4948
Anna Shadrina <ansshadrina@gmail.com>
2021-05-25 20:31:48 +0300

    Added a new test in LoginTests.

--- GmailTests/LoginTests.cs
+++ GmailTests/LoginTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Threading;
 
@@ -9,7 +10,11 @@ namespace GmailTests
     public class loginTests 
     {
         private IWebDriver driver;
-
+        string userName = "an_shadrina@mail.ru";
+        string password = "Sh1524";
+        string falseUserName = "lkenlanlcanlecna";
+        string errorMessage = "Неверное имя ящика";
+   
         [OneTimeSetUp]
         public void Init()
         {
@@ -32,11 +37,40 @@ namespace GmailTests
         [Test]
         public void Test1()
         {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             INavigation navigation = driver.Navigate();
-            navigation.GoToUrl("https://www.google.com/intl/de/gmail/about/#");
-            IWebElement logInButton = driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div[4]/ul[1]/li[2]/a"));
-            logInButton.Click();
-            Assert.Pass();
+            navigation.GoToUrl("https://mail.ru/");
+            IWebElement userNameField = driver.FindElement(LoginLockators.userNameFieldXpath);
+            userNameField.SendKeys(userName);
+            IWebElement furtherButton = driver.FindElement(LoginLockators.furtherButtonXpath);
+            furtherButton.Click();
+            IWebElement passwordField = driver.FindElement(LoginLockators.passwordFieldXpath);
+            passwordField.SendKeys(password);
+            IWebElement loginButton = driver.FindElement(LoginLockators.loginButtonXpath);
+            loginButton.Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            var res = wait.Until((d) => { return d.FindElement(LoginLockators.accountBlockXpath); });
+            IWebElement loginProof = driver.FindElement(LoginLockators.accountBlockXpath));
+            string proof = loginProof.GetAttribute("aria-label");
+            Assert.AreEqual(userName, proof);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            INavigation navigation = driver.Navigate();
+            navigation.GoToUrl("https://mail.ru/");
+            IWebElement userNameField = driver.FindElement(LoginLockators.userNameFieldXpath) ;
+            userNameField.SendKeys(falseUserName);
+            IWebElement furtherButton = driver.FindElement(LoginLockators.furtherButtonXpath);
+            furtherButton.Click();
+            var startTime = DateTime.UtcNow;
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+            wait.Until((d) => { return DateTime.UtcNow - startTime > TimeSpan.FromSeconds(1); }); 
+            IWebElement error = driver.FindElement(LoginLockators.errorMessage);
+            string borderColor = userNameField.GetCssValue("border-color");
+            Assert.AreEqual("rgb(244, 78, 78)", borderColor);
+            Assert.AreEqual(errorMessage,error.Text);
         }
     }
 }
